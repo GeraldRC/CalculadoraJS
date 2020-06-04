1a4803b76ea7a8cd9c4e0553fa6997ad01dea8ed
GeraldRC <gerald.reyes@outlook.cl>
2020-06-04 15:27:02 -0400

    codificacion archivo JavaScript
    Se crean las variables globales, para posterirormente ser usadas dentro del algoritmo.

---
+++ js/main.js
@@ -0,0 +1,26 @@
+/** variables para obtener acceso a botones */
+var num1 = document.querySelector('#num1');
+var num2 = document.querySelector('#num2');
+var num3 = document.querySelector('#num3');
+var num4 = document.querySelector('#num4');
+var num5 = document.querySelector('#num5');
+var num6 = document.querySelector('#num6');
+var num7 = document.querySelector('#num7');
+var num8 = document.querySelector('#num8');
+var num9 = document.querySelector('#num9');
+var num0 = document.querySelector('#numcero');
+var mul = document.querySelector('#multiplicar');
+var divi = document.querySelector('#dividir');
+var suma = document.querySelector('#sumar');
+var resta = document.querySelector('#restar');
+var clear = document.querySelector('#clear');
+var tot = document.querySelector('#tot');
+var input = document.querySelector('#input1');
+
+
+/** variables globales para guardar valores ingresados */
+var operando1 =0;
+var operando2 =0;
+var operador ="";
+var resultado =0;
+
