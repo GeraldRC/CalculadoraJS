d9e16d36c1d570f0fe4604529f3eed5b65cf805c
GeraldRC <gerald.reyes@outlook.cl>
2020-06-04 15:42:00 -0400

    codificacion de eventos
    se crean eventos de escucha a travez del click ,para luego iniciar una funcion que pueda insertar el valor correspondiente en la caja de texto en el documento html

--- js/main.js
+++ js/main.js
@@ -24,3 +24,34 @@ var operando2 =0;
 var operador ="";
 var resultado =0;
 
+/*funciones para pintar numeros en el input */
+num0.addEventListener("click",function(){
+    input.value+="0";
+});
+num1.addEventListener("click",function(){
+    input.value+="1";
+});
+num2.addEventListener("click",function(){
+    input.value+="2";
+});
+num3.addEventListener("click",function(){
+    input.value+="3";
+});
+num4.addEventListener("click",function(){
+    input.value+="4";
+});
+num5.addEventListener("click",function(){
+    input.value+="5";
+});
+num6.addEventListener("click",function(){
+    input.value+="6";
+});
+num7.addEventListener("click",function(){
+    input.value+="7";
+});
+num8.addEventListener("click",function(){
+    input.value+="8";
+});
+num9.addEventListener("click",function(){
+    input.value+="9";
+});
