befcf0df77afcc344813123be405552ac062493f
GeraldRC <gerald.reyes@outlook.cl>
2020-06-12 01:32:56 -0400

    add funciones
    se agregaron las funciones para que finalmente la calculadora pudiera hacer los calculos

--- js/main.js
+++ js/main.js
@@ -55,3 +55,85 @@ num8.addEventListener("click",function(){
 num9.addEventListener("click",function(){
     input.value+="9";
 });
+
+
+//funciones para recibir los datos y el primer operando
+mul.addEventListener("click",function(){
+    operando1= input.value;
+    operador ="x";
+    input.value="";
+});
+
+divi.addEventListener("click",function(){
+    operando1= input.value;
+    operador ="/";
+    input.value="";
+});
+
+suma.addEventListener("click",function(){
+    operando1= input.value;
+    operador ="+";
+    input.value="";
+});
+
+resta.addEventListener("click",function(){
+    operando1= input.value;
+    operador ="-";
+    input.value="";
+});
+
+
+//funcion para limpiar el input
+clear.addEventListener("click",function() {
+    input.value="";
+});
+
+/*funcion para obtner el segundo operando y realizar 
+ la evaluacion para ver que tipo de operacion realiazar*/
+tot.addEventListener("click",function(){
+    operando2 = input.value;
+    switch (operador) {
+        case "+":
+            sumar(operando1,operando2);
+            break;
+        case "-":
+            restar(operando1,operando2);
+            break;
+        case "x":
+            multiplicar(operando1,operando2);
+            break;
+        case "/":
+            dividir(operando1,operando2)
+            break;
+        default:
+            alert("ingrese  numeros")
+            break;
+    }
+}); 
+
+
+/*funciones para realizar operaciones matematicas */
+function sumar(n1,n2){
+    input.value="";
+    resultado = parseInt(n1) + parseInt(n2);
+    input.value= resultado;
+}
+
+function restar(n1,n2){
+    input.value="";
+    resultado = parseInt(n1) - parseInt(n2);
+    input.value = resultado;
+}
+
+function multiplicar(n1,n2){
+    input.value="";
+    resultado = parseInt(n1) * parseInt(n2);
+    input.value = resultado;
+}
+
+function dividir(n1,n2){
+    input.value="";
+    resultado = parseInt(n1) / parseInt(n2);
+    input.value = resultado;
+}
+
